a8b8cb76084186f7dcc51c7f08dbbf8fda8b76fb
robert lowry <lowryrob2001@gmail.com>
2019-11-04 14:46:39 -0800

    Add project files.

---
+++ TriviaAPIConsumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TriviaAPIConsumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Retrieving trivia questions...");
+
+            TriviaClient trivia = new TriviaClient();
+            string result = await trivia.GetTriviaQuestions();
+
+            Console.WriteLine("Received trivia questions. \n\n");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TriviaAPIConsumer/TriviaClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaAPIConsumer
+{
+    // Trivia API: https://opentdb.com/api_config.php
+    public class TriviaClient
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        // Static constructor; Runs once per class
+        static TriviaClient()
+        {
+            // Initialize all HttpClient settings
+        }
+
+        public async Task<string> GetTriviaQuestions()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://opentdb.com/api.php?amount=5");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                return data;
+            }
+            else
+            {
+                // If not successful, null is returned
+                return null;
+            }
+        }
+    }
+
+    public class Result
+    {
+        public string category { get; set; }
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+        public List<string> incorrect_answers { get; set; }
+    }
+
+    public class TriviaResponse
+    {
+        public int response_code { get; set; }
+        public List<Result> results { get; set; }
+    }
+}
